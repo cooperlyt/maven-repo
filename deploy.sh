f7369d98643245a0287a1bb50cf52466403187de
cooper <lyt3131@163.com>
2021-08-26 11:43:35 +0800

    commit new

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo "    3、参数说明：r/s r是release正式版本，s是snapshot快照版本"
 
 #deploy参数，snapshot 表示快照包，简写为s， release表示正式包，简写为r
-set arg=%1
+arg=%1
 
 set branch='release'
 
@@ -18,11 +18,11 @@ then
 fi
 
 #正式包发布 release分支
-echo 本地存储库的发送到github仓库%branch%分支上
+echo 本地存储库的发送到github仓库$branch分支上
 git add .
 git commit -m "commit new"
 git pull
-git push origin %branch%
+git push origin $branch
 
 echo 将%branch%分支合并到master分支
 git checkout master
@@ -32,5 +32,5 @@ git merge %branch%
 git commit -m 'master merge'
 git push origin master
 
-pause
+echo complete
 
